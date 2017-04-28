860ff6f4a51d32a50e0f9a40ac7355bcd31f2bcd
Jon Lindell <jhlindell@yahoo.com>
2017-04-28 15:55:15 -0600

    preliminary merge

--- script.js
+++ script.js
@@ -1,17 +1,69 @@
-
 var map;
-var latLng = {lat: -25.363, lng: 131.044};
-      function initMap() {
+var latLng = {
+  lat: -25.363,
+  lng: 131.044
+};
 
-        console.log(document.getElementById('map'));
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 2
-        });
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 2
+  });
 
-        var marker = new google.maps.Marker({
-          position: latLng,
-          map: map,
-          title: 'Hello World!'
-        });
-      }
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+
+var $body = $('Body');
+
+function getData (event) {
+  event.preventDefault();
+  let $startDate = $('#startDate').val();
+  let $endDate = $('#endDate').val();
+  let $magnitude = $('#magnitude').val();
+  let string = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime='+ $startDate + '&endtime=' + $endDate + '&minmagnitude=' + $magnitude;
+
+  var $xhr = $.getJSON(string);
+
+  $xhr.done(function(data) {
+      //console.log(data.features);
+      $.each(data.features, function(){
+        let $element = $('<div>');
+        let $time = new Date(this.properties.time);
+        let $mag = this.properties.mag;
+        let $place = this.properties.place;
+        let $lat = this.geometry.coordinates[1];
+        let $long = this.geometry.coordinates[0];
+        let $placetag = $('<p>');
+        $placetag.append($('<span>').html('Location: '));
+        $placetag.append($('<span>').html($place));
+        $element.append($placetag);
+        let $timetag = $('<p>');
+        $timetag.append($('<span>').html('Time: '));
+        $timetag.append($('<span>').html($time));
+        $element.append($timetag);
+        let $magtag = $('<p>');
+        $magtag.append($('<span>').html('Magnitude: '));
+        $magtag.append($('<span>').html($mag));
+        $element.append($magtag);
+        let $coords = $('<p>');
+        $coords.append($('<span>').html('Lattitude: '));
+        $coords.append($('<span>').html($lat));
+        $coords.append($('<span>').html('Longitude: '));
+        $coords.append($('<span>').html($long));
+        $element.append($coords);
+        $body.append($element);
+      });
+  });
+  $xhr.fail(function(err) {
+      console.log(err);
+  });
+}
+
+$('#userInput').on('submit', getData);
